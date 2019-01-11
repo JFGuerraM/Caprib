fd181c16d8279b43621e8b615e96b6c0f2d1917b
Juan <juan_guerra@hotmail.com>
2019-01-11 10:07:56 -0500

    bash updated

---
+++ externalPrograms/cleanAfterBlast.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+rm -r *.fasta
+rm -r *.fa
+rm -r *.dna
+rm -r *.txt
+rm -r *fna
+
+cd ..
+rm -r *.nhr
+rm -r *.nin
+rm -r *.nsd
+rm -r *.nsi
+rm -r *.nsq
+rm -r *.nni
+rm -r *.nnd
+rm -r *.nog
+
