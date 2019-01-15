4cd3d6a4c410a0d77c483f62aacb29ef9474b73e
Juan <juan_guerra@hotmail.com>
2019-01-15 16:03:56 -0500

    sh updated

--- externalPrograms/cleanAfterBlast.sh
+++ externalPrograms/cleanAfterBlast.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-rm -r *.fasta
-rm -r *.fa
-rm -r *.dna
-rm -r *.txt
-rm -r *fna
-
-cd ..
 rm -r *.nhr
 rm -r *.nin
 rm -r *.nsd
@@ -16,3 +9,9 @@ rm -r *.nni
 rm -r *.nnd
 rm -r *.nog
 
+cd externalPrograms
+rm -r *.fasta
+rm -r *.fa
+rm -r *.dna
+rm -r *.txt
+rm -r *fna
